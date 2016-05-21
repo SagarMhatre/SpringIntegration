28b87dd34fedc00f9bec4adf9b0bcf21e57e088a
Sagar Mhatre <sagarmmhatre@yahoo.co.in>
2016-05-21 20:31:16 +0530

    Getting messages from Active MQ
    * Declare a bean for connectionFactory, ActiveMQQueue, a queue (channel
    with capacity) & map the 2 in a message-driven-channel-adapter

--- src/main/java/in/mhatre/sagar/spring/integration/BasicJMS.java
+++ src/main/java/in/mhatre/sagar/spring/integration/BasicJMS.java
@@ -3,6 +3,7 @@ package in.mhatre.sagar.spring.integration;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.PollableChannel;
 import org.springframework.messaging.support.GenericMessage;
 
 public class BasicJMS {
@@ -11,5 +12,8 @@ public class BasicJMS {
 		AbstractApplicationContext context = new ClassPathXmlApplicationContext("jms.xml");
 		MessageChannel inputChannel = context.getBean("stdinToJmsoutChannel", MessageChannel.class);		
 		inputChannel.send(new GenericMessage<String>("Hello JMS World"));
+		
+		PollableChannel outputChannel = context.getBean("jmsInChannel", PollableChannel.class);		
+		System.out.println("Received from JMS : " + outputChannel.receive(0).getPayload());
 	}
 }
